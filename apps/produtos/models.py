ecf1ab31fdaa516378b83f9c0f99b20d30448955
LancelloT7 <matias7g@hotmail.com>
2025-01-17 18:19:44 -0300

    Ajustes Front End

--- apps/produtos/models.py
+++ apps/produtos/models.py
@@ -5,7 +5,9 @@ class Produto(models.Model):
     nome = models.CharField(max_length=100)
     formula = models.CharField(max_length=100)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
-    disponivel = models.BooleanField(default=True) 
+    disponivel = models.BooleanField(default=True)
+    img = models.ImageField(upload_to='produtos/')
+
     
     def __str__(self):
         return f'{self.nome}, {self.formula}, {self.preco}, {self.disponivel}'
